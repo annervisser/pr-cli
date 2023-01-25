7450ba46ec634eb12dd168e9d63d39949cfaef5a
Anner Visser <mail@annervisser.nl>
2023-01-25 18:54:11 +0100

    Fix being unable to override pull/push remotes

--- commands/pick/pick-command.ts
+++ commands/pick/pick-command.ts
@@ -42,8 +42,8 @@ export const pickCommand = new Command()
 			}
 		}
 
-		options.pullRemote = await getPullRemote();
-		options.pushRemote = await getPushRemote();
+		options.pullRemote ??= await getPullRemote();
+		options.pushRemote ??= await getPushRemote();
 
 		log.debug(options);
 
