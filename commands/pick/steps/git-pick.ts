3797943486a25b65b566fc29bc19c3c6294cdd75
Anner Visser <mail@annervisser.nl>
2023-02-12 21:03:32 +0100

    Cleanup temporary worktree on failure
    Fixes #31

--- commands/pick/steps/git-pick.ts
+++ commands/pick/steps/git-pick.ts
@@ -21,45 +21,62 @@ export interface GitPickSettings {
 }
 
 export async function runCherryPick(settings: GitPickSettings): Promise<void> {
-	log.info(colors.green('▶️ Creating temporary directory'));
-	const tmpDir = await runAndCapture('mktemp', '-d');
+	const cleanupSteps: Array<{ message: string; action: () => void | Promise<void> }> = [];
 
-	log.info(colors.green(`▶️ Creating temporary worktree in ${tmpDir}`));
-	const upstreamRef = `${settings.pullRemote}/${settings.upstreamBranch}`;
-	await runCommand(
-		'git',
-		'worktree',
-		'add',
-		'--no-track',
-		settings.overwriteLocalBranch ? '-B' : '-b',
-		settings.branchName,
-		tmpDir,
-		upstreamRef,
-	);
-
-	Deno.chdir(tmpDir);
-
-	log.info(colors.green('▶️ cherry picking commits'));
-	const commitSHAsToPick = settings.commits.map((c) => c.sha);
-	await runCommand('git', 'cherry-pick', ...commitSHAsToPick);
-
-	if (settings.push) {
-		log.info(colors.green(`▶️ Pushing to ${settings.pushRemote}/${settings.branchName}`));
-		const args = ['-u', settings.pushRemote];
-		settings.forcePush && args.push('--force');
-		await runCommand('git', 'push', ...args, settings.branchName);
-	}
-
-	if (settings.pr) {
-		log.info(colors.green('▶️ Creating pull request'));
-		// Check if a pr is already open, using: `gh api "/repos/{owner}/{repo}/pulls?state=all&head={owner}:install-deps-command-for-gum" -q ".[] | {url}"`
-		await GH.createPullRequest({
-			baseBranch: settings.upstreamBranch,
-			draftPR: settings.draftPR,
+	try {
+		log.info(colors.green('▶️ Creating temporary directory'));
+		const tmpDir = await runAndCapture('mktemp', '-d');
+		cleanupSteps.push({
+			message: colors.green(`▶️ Deleting temporary directory ${tmpDir}`),
+			action: async () => await runCommand('rmdir', tmpDir),
 		});
-	}
 
-	// TODO still do this if an error occurs
-	log.info(colors.green('▶️ Cleaning up temporary worktree'));
-	await runCommand('git', 'worktree', 'remove', tmpDir);
+		log.info(colors.green(`▶️ Creating temporary worktree in ${tmpDir}`));
+		const upstreamRef = `${settings.pullRemote}/${settings.upstreamBranch}`;
+		await runCommand(
+			'git',
+			'worktree',
+			'add',
+			'--no-track',
+			settings.overwriteLocalBranch ? '-B' : '-b',
+			settings.branchName,
+			tmpDir,
+			upstreamRef,
+		);
+		cleanupSteps.push({
+			message: colors.green('▶️ Cleaning up temporary worktree'),
+			action: async () => await runCommand('git', 'worktree', 'remove', tmpDir),
+		});
+
+		Deno.chdir(tmpDir);
+
+		log.info(colors.green('▶️ cherry picking commits'));
+		const commitSHAsToPick = settings.commits.map((c) => c.sha);
+		await runCommand('git', 'cherry-pick', ...commitSHAsToPick);
+
+		if (settings.push) {
+			log.info(colors.green(`▶️ Pushing to ${settings.pushRemote}/${settings.branchName}`));
+			const args = ['-u', settings.pushRemote];
+			settings.forcePush && args.push('--force');
+			await runCommand('git', 'push', ...args, settings.branchName);
+		}
+
+		if (settings.pr) {
+			log.info(colors.green('▶️ Creating pull request'));
+			// Check if a pr is already open, using: `gh api "/repos/{owner}/{repo}/pulls?state=all&head={owner}:install-deps-command-for-gum" -q ".[] | {url}"`
+			await GH.createPullRequest({
+				baseBranch: settings.upstreamBranch,
+				draftPR: settings.draftPR,
+			});
+		}
+	} finally {
+		for (const { message, action } of cleanupSteps.reverse()) {
+			log.info(message);
+			try {
+				await action();
+			} catch {
+				// ignore
+			}
+		}
+	}
 }
