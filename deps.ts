357012c8a496770c62a7af329c1c1bf24053f20d
Anner Visser <mail@annervisser.nl>
2023-09-17 13:11:36 +0200

    Update all Deno dependencies

--- deps.ts
+++ deps.ts
@@ -1,11 +1,11 @@
-export * as log from 'https://deno.land/std@0.192.0/log/mod.ts';
-export * from 'https://deno.land/x/cliffy@v0.25.7/command/mod.ts';
-export * from 'https://deno.land/x/cliffy@v0.25.7/table/table.ts';
-export * from 'https://deno.land/x/cliffy@v0.25.7/flags/flags.ts';
-export * from 'https://deno.land/x/cliffy@v0.25.7/prompt/mod.ts';
-export { colors } from 'https://deno.land/x/cliffy@v0.25.7/ansi/colors.ts';
-export { slug } from 'https://deno.land/x/slug@v1.1.0/mod.ts';
+export * as log from 'https://deno.land/std@0.201.0/log/mod.ts';
+export { ensureDir } from 'https://deno.land/std@0.201.0/fs/ensure_dir.ts';
+export * as path from 'https://deno.land/std@0.201.0/path/mod.ts';
+export * as semver from 'https://deno.land/std@0.201.0/semver/mod.ts';
 
-export { ensureDir } from 'https://deno.land/std@0.173.0/fs/ensure_dir.ts';
-export * as path from 'https://deno.land/std@0.173.0/path/mod.ts';
-export * as semver from 'https://deno.land/std@0.173.0/semver/mod.ts';
+export * from 'https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts';
+export * from 'https://deno.land/x/cliffy@v1.0.0-rc.3/table/table.ts';
+export * from 'https://deno.land/x/cliffy@v1.0.0-rc.3/flags/flags.ts';
+export * from 'https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/mod.ts';
+export { colors } from 'https://deno.land/x/cliffy@v1.0.0-rc.3/ansi/colors.ts';
+export { slug } from 'https://deno.land/x/slug@v1.1.0/mod.ts';
