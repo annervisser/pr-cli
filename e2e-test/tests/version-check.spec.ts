30e4d75c02f10d054550739c9ce62554197276ce
Anner Visser <mail@annervisser.nl>
2023-09-17 23:20:32 +0200

    Change version check test to support versions with a suffix

--- e2e-test/tests/version-check.spec.ts
+++ e2e-test/tests/version-check.spec.ts
@@ -3,6 +3,6 @@ import { test } from '@helpers/cli-test';
 
 test('Version check', async ({ cli }) => {
 	await cli.command('pr-cli --version');
-	await expect(cli.lineN(1).getByText(/^pr-cli [0-9.]+$/)).toBeVisible();
+	await expect(cli.lineN(1).getByText(/^pr-cli [0-9.]{5,}/)).toBeVisible();
 	await cli.assertExitCode(0);
 });
