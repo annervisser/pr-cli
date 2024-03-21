c86f234588a32817f48e378e0ad1fe356519e202
Anner Visser <mail@annervisser.nl>
2024-03-21 20:24:43 +0100

    Add update-pull-request command that runs pick on an existing PR

---
+++ commands/update-pull-request/update-pull-request-command.ts
@@ -0,0 +1,35 @@
+import { Command } from '../../deps.ts';
+import { GH } from '../../lib/github/gh.ts';
+import { checkDependencies } from '../pick/steps/check-dependencies.ts';
+import { chooseOneFormatted } from '../../lib/pr-cli/choose.ts';
+import { pickCommand } from '../pick/pick-command.ts';
+
+export const updatePullRequestCommand = new Command()
+	.name('update-pull-request')
+	.alias('upr')
+	.description('Update a pull request by force pushing some local commits')
+	.action(async () => {
+		await checkDependencies();
+
+		const prs = await GH.listPullRequests();
+		if (prs.length < 1) {
+			throw new Error('No pull requests to update');
+		}
+
+		const pr = await chooseOneFormatted(
+			prs,
+			(pr) => pr.title,
+			{
+				header: 'What PR do you want to update?',
+			},
+		);
+
+		await pickCommand.parse([
+			'--branch',
+			pr.headRefName,
+			'--base',
+			pr.baseRefName,
+			'--title',
+			pr.title,
+		]);
+	});
