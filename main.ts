140a13113bab763011970d5cdeea05857cbd1c0f
Anner Visser <mail@annervisser.nl>
2024-09-20 10:29:28 +0200

    Bump version to 1.9.0

--- main.ts
+++ main.ts
@@ -13,7 +13,7 @@ import { CommandExecutionError } from './lib/shell/command-execution-error.ts';
 if (import.meta.main) {
 	const main = new Command()
 		.name('pr-cli')
-		.version('1.8.0')
+		.version('1.9.0')
 		.option('--debug', 'enable verbose error logging', { global: true })
 		.description(
 			'Command line utility for quickly creating pull requests on Github',
